b429d14f2f47f86e968fb7373bebb503e31ac4eb
reyhades <reyhades2005@gmail.com>
2024-10-04 11:37:12 -0600

    V1.view

--- src/app/component/movie-card/movie-card.component.ts
+++ src/app/component/movie-card/movie-card.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [HttpClientModule, CommonModule], // Agrega CommonModule aquí
+  imports: [HttpClientModule, CommonModule], 
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent implements OnInit {
-  movies: any[] = []; // Array para almacenar las películas
+  movies: any[] = []; 
 
   constructor(private http: HttpClient) {}
 
@@ -22,7 +22,7 @@ export class MovieCardComponent implements OnInit {
     this.http.get<any[]>('http://localhost:3000/api/peliculas')
       .subscribe(
         (data) => {
-          console.log(data); // Verifica que los datos sean correctos
+          console.log(data);
           this.movies = data;
         },
         (error) => {
